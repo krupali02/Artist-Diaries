c9fdd0cbe8adbde581e1f33ba02eeb19a1f805c6
krupali02 <70813233+krupali02@users.noreply.github.com>
2024-01-25 17:47:02 -0500

    Updates

---
+++ img/src/console_apps/HelloWorld.java
@@ -0,0 +1,10 @@
+package console_apps;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello EECS1022 W21!");
+		System.out.println("Hello Krupali!");
+	}
+
+}
---
+++ website.js
@@ -0,0 +1,63 @@
+function a () {
+    document.getElementById("introo").style.display= 'none'
+    document.getElementById("leoo").style.display= 'block'
+    document.getElementById("jac").style.display= 'none'
+    document.getElementById("vin").style.display= 'none'
+    document.getElementById("pab").style.display= 'none'
+    document.getElementById("rem").style.display= 'none'
+    document.getElementById("wal").style.display= 'none'
+
+}
+
+function b () {
+    document.getElementById("introo").style.display= 'none'
+    document.getElementById("leoo").style.display= 'none'
+    document.getElementById("jac").style.display= 'block'
+    document.getElementById("vin").style.display= 'none'
+    document.getElementById("pab").style.display= 'none'
+    document.getElementById("rem").style.display= 'none'
+    document.getElementById("wal").style.display= 'none'
+
+}
+
+function c () {
+    document.getElementById("vin").style.display= 'block'
+    document.getElementById("introo").style.display= 'none'
+    document.getElementById("leoo").style.display= 'none'
+    document.getElementById("jac").style.display= 'none'
+    document.getElementById("pab").style.display= 'none'
+    document.getElementById("rem").style.display= 'none'
+    document.getElementById("wal").style.display= 'none'
+
+}
+
+function d () {
+    document.getElementById("introo").style.display= 'none'
+    document.getElementById("leoo").style.display= 'none'
+    document.getElementById("jac").style.display= 'none'
+    document.getElementById("vin").style.display= 'none'
+    document.getElementById("pab").style.display= 'block'
+    document.getElementById("rem").style.display= 'none'
+    document.getElementById("wal").style.display= 'none'
+}
+
+function e () {
+    document.getElementById("introo").style.display= 'none'
+    document.getElementById("leoo").style.display= 'none'
+    document.getElementById("jac").style.display= 'none'
+    document.getElementById("vin").style.display= 'none'
+    document.getElementById("pab").style.display= 'none'
+    document.getElementById("rem").style.display= 'block'
+    document.getElementById("wal").style.display= 'none'
+}
+
+function f () {
+    document.getElementById("introo").style.display= 'none'
+    document.getElementById("leoo").style.display= 'none'
+    document.getElementById("jac").style.display= 'none'
+    document.getElementById("vin").style.display= 'none'
+    document.getElementById("pab").style.display= 'none'
+    document.getElementById("rem").style.display= 'none'
+    document.getElementById("wal").style.display= 'block'
+}
+
